5e589a7afa4d04fea53a77b1da5cf21967feb64b
Morgan-Sell <morganpsell@gmail.com>
2024-10-26 14:07:45 -0700

    added error handling to fetch_weather_api(). need to create unit tests.

--- src/api.py
+++ src/api.py
@@ -1,5 +1,6 @@
 import requests
 
+
 import json
 
 from src.config import VISUAL_CROSSING_API_URL
@@ -19,10 +20,22 @@ def fetch_weather_api(endpoint: str, location: str, unit: str, api_key: str) -> 
         "contentType": "json",
         "key": api_key
     }
-    response = requests.get(base_url, params)
 
-    if response.status_code == 200:
-        data = response.json()
-        return data
-    else:
-        print("Failed to fetch data: ", response.status_code, response.text)
+    try:
+        response = requests.get(base_url, params)
+        response.raise_for_status() # raises an error  for 4xx/5xx responses
+    except requests.exceptions.ConnectionError:
+        raise ConnectionError("Failed to connect to the API. Check your network connection.")
+    except requests.exceptions.Timeout:
+        raise TimeoutError("The request timed out. Try again later.")
+    except requests.exceptions.HTTPError as http_err:
+        if response.status_code == 400:
+            raise ValueError("Invalid parameters were provided to the API.") from http_err
+        elif response.status_code == 401:
+            raise PermissionError("Invalid API key.") from http_err
+        else:
+            raise RuntimeError(f"HTTP error occured: {response.status_code}") from http_err
+    except requests.exceptions.RequestException as e:
+        raise RuntimeError("An unexpected error occurred with the request.") from e
+    
+    return response.json()
--- tests/test_api.py
+++ tests/test_api.py
@@ -1,11 +1,13 @@
 import json
 from unittest.mock import MagicMock, patch
 
-
+import requests
 from src.api import fetch_weather_api
 import urllib
 
+from src.config import VISUAL_CROSSING_API_URL
 
+import pytest
 
 @patch("urllib.request.urlopen")
 def test_fetch_weather_api_success(mock_urlopen, weather_api_data):
@@ -27,3 +29,18 @@ def test_fetch_weather_api_success(mock_urlopen, weather_api_data):
     assert isinstance(data, dict)
     assert data["location"] == location
     assert len(data["location"]["values"]) == 3
+
+
+@patch("urllib.request.urlopen")
+def test_fetch_weather_api_connection_error(mock_urlopen):
+    # Arrange: mock a failed response
+    mock_urlopen.side_effect = ConnectionError("Failed to connect to the API. Check your network connection.")
+
+    endpoint = "forecast"
+    location = "Buenos Aires"
+    unit = "metric"
+    api_key = "secret"
+
+    # Action & Assert
+    with pytest.raises(ConnectionError):
+        fetch_weather_api(endpoint, location, unit, api_key)
