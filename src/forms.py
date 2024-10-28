0f1bb938a9f9aac7440a7d6fdfd1765af4019870
Morgan-Sell <morganpsell@gmail.com>
2024-10-28 16:59:30 -0700

    started to develop frontend

---
+++ src/forms.py
@@ -0,0 +1,8 @@
+from flask_wtf import FlaskForm
+from wtforms import SubmitField, StringField
+from wtforms.validators import DataRequired
+
+
+class LocationForm(FlaskForm):
+    location = StringField("Enter any city in the world: ", validators=[DataRequired()])
+    convert = SubmitField(label="Collect Weather Data")
